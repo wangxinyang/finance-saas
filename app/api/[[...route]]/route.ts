525f5d26e3a92a45cdf6233957ee7e084723d59c
wangxinyang1983@gmail.com <359387050@qq.com>
2024-06-05 15:41:50 +0900

    Hono setpu

---
+++ app/api/[[...route]]/route.ts
@@ -0,0 +1,24 @@
+import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+
+export const runtime = "edge";
+
+const app = new Hono().basePath("/api");
+
+app.get("/hello", clerkMiddleware(), (c) => {
+  const auth = getAuth(c);
+
+  if (!auth?.userId) {
+    return c.json({
+      message: "You are not logged in!",
+    });
+  }
+
+  return c.json({
+    message: "Hello Next.js!",
+  });
+});
+
+export const GET = handle(app);
+export const POST = handle(app);
