364da46d440937aee7943ce3b499b051c939d210
wangxinyang1983@gmail.com <359387050@qq.com>
2024-06-06 08:40:22 +0900

    complete the header component

--- components/header.tsx
+++ components/header.tsx
@@ -1,5 +1,9 @@
+import { Loader2 } from "lucide-react";
+import { ClerkLoaded, ClerkLoading, UserButton } from "@clerk/nextjs";
+
 import { HeaderLogo } from "@/components/header-logo";
 import { Navigation } from "@/components/navigation";
+import { WelcomeMsg } from "@/components/welcome-msg";
 
 export const Header = () => {
   return (
@@ -10,7 +14,14 @@ export const Header = () => {
             <HeaderLogo />
             <Navigation />
           </div>
+          <ClerkLoaded>
+            <UserButton afterSignOutUrl="/" />
+          </ClerkLoaded>
+          <ClerkLoading>
+            <Loader2 className="size-8 animate-spin text-slate-400" />
+          </ClerkLoading>
         </div>
+        <WelcomeMsg />
       </div>
     </header>
   );
