926702dd86e3ce60f668eaeeee96249d3db0697b
wangxinyang1983@gmail.com <359387050@qq.com>
2024-06-06 22:52:54 +0900

    add new account form component

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,7 +1,15 @@
+"use client";
+
+import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
+
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
+  const { onOpen } = useNewAccount();
+
   return (
     <div>
-      <p>Home</p>
+      <Button onClick={onOpen}>Add an account</Button>
     </div>
   );
 }
---
+++ features/accounts/components/new-account-sheet.tsx
@@ -0,0 +1,42 @@
+import { z } from "zod";
+
+import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
+import { AccountForm } from "@/features/accounts/components/account-form";
+
+import { insertAccountSchema } from "@/db/schema";
+
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+
+const formSchema = insertAccountSchema.pick({
+  name: true,
+});
+
+type FormValues = z.input<typeof formSchema>;
+
+export const NewAccountSheet = () => {
+  const { isOpen, onClose } = useNewAccount();
+
+  const onSubmit = (values: FormValues) => {
+    console.log(values);
+  };
+
+  return (
+    <Sheet open={isOpen} onOpenChange={onClose}>
+      <SheetContent className="space-y-4">
+        <SheetHeader>
+          <SheetTitle>New Account</SheetTitle>
+          <SheetDescription>
+            Create a new account to track your transactions.
+          </SheetDescription>
+        </SheetHeader>
+        <AccountForm onSubmit={onSubmit} disabled={false} />
+      </SheetContent>
+    </Sheet>
+  );
+};
