2b44e03e4ad12a0035aaefacdd4ef27db73196cd
wangxinyang1983@gmail.com <359387050@qq.com>
2024-06-07 09:29:11 +0900

    reuse account form

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 
+import { Toaster } from "@/components/ui/sonner";
 import { QueryProvider } from "@/providers/query-provider";
 import { SheetProvider } from "@/providers/sheet-provider";
 
@@ -24,6 +25,7 @@ export default function RootLayout({
         <body className={inter.className}>
           <QueryProvider>
             <SheetProvider />
+            <Toaster />
             {children}
           </QueryProvider>
         </body>
--- features/accounts/components/new-account-sheet.tsx
+++ features/accounts/components/new-account-sheet.tsx
@@ -1,6 +1,7 @@
 import { z } from "zod";
 
 import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
+import { useCreateAccount } from "@/features/accounts/api/use-create-accounts";
 import { AccountForm } from "@/features/accounts/components/account-form";
 
 import { insertAccountSchema } from "@/db/schema";
@@ -22,8 +23,14 @@ type FormValues = z.input<typeof formSchema>;
 export const NewAccountSheet = () => {
   const { isOpen, onClose } = useNewAccount();
 
+  const mutation = useCreateAccount();
+
   const onSubmit = (values: FormValues) => {
-    console.log(values);
+    mutation.mutate(values, {
+      onSuccess: () => {
+        onClose();
+      },
+    });
   };
 
   return (
@@ -35,7 +42,11 @@ export const NewAccountSheet = () => {
             Create a new account to track your transactions.
           </SheetDescription>
         </SheetHeader>
-        <AccountForm onSubmit={onSubmit} disabled={false} />
+        <AccountForm
+          onSubmit={onSubmit}
+          disabled={mutation.isPending}
+          defaultValues={{ name: "" }}
+        />
       </SheetContent>
     </Sheet>
   );
