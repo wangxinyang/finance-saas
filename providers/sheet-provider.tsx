175e4dd0ebc2015ee04b5617c1aa42df44d7543e
wangxinyang1983@gmail.com <359387050@qq.com>
2024-06-09 09:29:13 +0900

    add edit hook and sheet

--- providers/sheet-provider.tsx
+++ providers/sheet-provider.tsx
@@ -1,14 +1,19 @@
 "use client";
 
-import { NewAccountSheet } from "@/features/accounts/components/new-account-sheet";
 import { useMountedState } from "react-use";
+
+import { EditAccountSheet } from "@/features/accounts/components/edit-account-sheet";
+import { NewAccountSheet } from "@/features/accounts/components/new-account-sheet";
 
 export const SheetProvider = () => {
   const isMounted = useMountedState();
 
+  if (!isMounted) return null;
+
   return (
     <>
       <NewAccountSheet />
+      <EditAccountSheet />
     </>
   );
 };
