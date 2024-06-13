fd9c38913851cf36c377680ee739b612161562d4
wangxinyang1983@gmail.com <359387050@qq.com>
2024-06-13 09:12:15 +0900

    feat: add transactions api and hooks

---
+++ features/transactions/api/use-bulk-create-transactions.ts
@@ -0,0 +1,34 @@
+import { toast } from "sonner";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { InferRequestType, InferResponseType } from "hono";
+
+import { client } from "@/lib/hono";
+
+type ResponseType = InferResponseType<
+  (typeof client.api.transactions)["bulk-create"]["$post"]
+>;
+type RequestType = InferRequestType<
+  (typeof client.api.transactions)["bulk-create"]["$post"]
+>["json"];
+
+export const useBulkDeleteTransactions = () => {
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation<ResponseType, Error, RequestType>({
+    mutationFn: async (json) => {
+      const response = await client.api.transactions["bulk-create"]["$post"]({
+        json,
+      });
+      return await response.json();
+    },
+    onSuccess: () => {
+      toast.success("Transactions created");
+      queryClient.invalidateQueries({ queryKey: ["transactions"] });
+    },
+    onError: () => {
+      toast.error("Failed to create transactions");
+    },
+  });
+
+  return mutation;
+};
---
+++ features/transactions/api/use-delete-transaction.ts
@@ -0,0 +1,33 @@
+import { toast } from "sonner";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { InferResponseType } from "hono";
+
+import { client } from "@/lib/hono";
+
+type ResponseType = InferResponseType<
+  (typeof client.api.transactions)[":id"]["$delete"]
+>;
+
+export const useDeleteTransaction = (id?: string) => {
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation<ResponseType, Error>({
+    mutationFn: async () => {
+      const response = await client.api.transactions[":id"]["$delete"]({
+        param: { id },
+      });
+      return await response.json();
+    },
+    onSuccess: () => {
+      toast.success("Transaction deleted");
+      queryClient.invalidateQueries({ queryKey: ["transaction", { id }] });
+      queryClient.invalidateQueries({ queryKey: ["transactions"] });
+      //TODO: Invalidate summary
+    },
+    onError: () => {
+      toast.error("Failed to delete transaction");
+    },
+  });
+
+  return mutation;
+};
