5f24dab0bcf827383a263e5d02cb06cbf9860b60
wangxinyang1983@gmail.com <359387050@qq.com>
2024-06-18 18:12:57 +0900

    fix: some todo comments

--- features/categories/api/use-bulk-delete-categories.ts
+++ features/categories/api/use-bulk-delete-categories.ts
@@ -24,6 +24,7 @@ export const useBulkDeleteCategories = () => {
     onSuccess: () => {
       toast.success("Categories deleted");
       queryClient.invalidateQueries({ queryKey: ["categories"] });
+      queryClient.invalidateQueries({ queryKey: ["summary"] });
     },
     onError: () => {
       toast.error("Failed to delete account");
